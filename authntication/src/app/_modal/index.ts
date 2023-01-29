c618c0aa055dcc3db870f308a9a4b9f54191d4ce
Manav <chavdamanav17@gmail.com>
2023-01-29 21:22:02 +0530

    Model And Validation Added

---
+++ authntication/src/app/_modal/index.ts
@@ -0,0 +1,2 @@
+export * from './modal.Module';
+export * from './modal.service';
--- authntication/src/app/component/authntication/authntication.component.ts
+++ authntication/src/app/component/authntication/authntication.component.ts
@@ -1,48 +1,95 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { LoginTokenService } from 'src/app/services/login-token.service';
-
+import { ModalService } from 'src/app/_modal';
 
 @Component({
   selector: 'app-authntication',
   templateUrl: './authntication.component.html',
-  styleUrls: ['./authntication.component.scss']
+  styleUrls: ['./authntication.component.scss'],
 })
 export class AuthnticationComponent implements OnInit {
-  fb:FormGroup;
-  register:FormGroup
+  fb: FormGroup;
+  register: FormGroup;
   isSignUp = false;
-  constructor(private loginService:LoginTokenService) { }
+  submitted = false;
+  loginsubmitted = false;
+  message: any;
+  constructor(
+    private loginService: LoginTokenService,
+    private formBuilder: FormBuilder,
+    private model: ModalService
+  ) {}
 
   ngOnInit(): void {
-    this.register = new FormGroup({
-      name: new FormControl('',[Validators.required]),
-      email: new FormControl('',[Validators.required , Validators.email]),
-      password: new FormControl('',[Validators.required])
-    })
-    this.fb = new FormGroup({
-      email: new FormControl('',[Validators.required,Validators.email]),
+    this.register = this.formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(10),
+      ]),
+    });
+    this.fb = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
-    })
+    });
   }
-  submit(){
-    if(!this.isSignUp){
-      let user = {
-        email:this.fb.get('email').value,
-        password:this.fb.get('password').value
+  submit() {
+    if (!this.isSignUp) {
+      this.loginsubmitted = true;
+      if (this.fb.invalid) {
+        return false;
+      } else {
+        let user = {
+          email: this.fb.get('email').value,
+          password: this.fb.get('password').value,
+        };
+        this.loginService.loginUser(user).subscribe(
+          (res: any) => {
+            if (res) {
+              console.log('res', res);
+              localStorage.setItem('token', JSON.stringify(res.token));
+              this.model.open('1');
+              this.message = res.msg;
+            }
+          },
+          (err) => {
+            if (err) console.log(err);
+            console.log('Success-login');
+          }
+        );
+        console.log('info', this.loginService.resMessage);
       }
-      this.loginService.loginUser(user);
-    }else{
-      let user = {
-        name:this.register.get('name').value,
-        email:this.register.get('email').value,
-        password:this.register.get('password').value
+    } else {
+      this.submitted = true;
+      if (this.register.invalid) {
+        return false;
+      } else {
+        let user = {
+          name: this.register.get('name').value,
+          email: this.register.get('email').value,
+          password: this.register.get('password').value,
+        };
+        this.loginService.registerUser(user);
       }
-      this.loginService.registerUser(user)
     }
   }
-  singUpMode(){
-    console.log('worked')
+  singUpMode() {
+    console.log('worked');
     this.isSignUp = !this.isSignUp;
+  }
+  get r(): { [key: string]: AbstractControl } {
+    return this.register.controls;
+  }
+  get l(): { [key: string]: AbstractControl } {
+    return this.fb.controls;
   }
 }
